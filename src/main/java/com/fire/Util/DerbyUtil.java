8e9b88d873428e66e171a82463bdcf642697d0db
jiefeng <jiefengfire@163.com>
2019-11-15 15:16:55 +0800

    配置了一下数据库

--- src/main/java/com/fire/Util/DerbyUtil.java
+++ src/main/java/com/fire/Util/DerbyUtil.java
@@ -5,15 +5,19 @@ import java.util.Properties;
 
 public class DerbyUtil {
     public static Connection getConn() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+//        驱动
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
             System.out.println("Loaded the EmbeddedDriver");
             Connection conn = null;
             Properties props = new Properties();
+//            配置
             props.setProperty("user", "user");
             props.setProperty("password", "password");
             try {
+//                如果没有会创建，传入配置信息
                 conn = DriverManager.getConnection("jdbc:derby:helloDB;create=true", props);
                 System.out.println("create derbyDB");
+//                关闭自动commit
                 conn.setAutoCommit(false);
 
             } catch (SQLException e) {
@@ -23,6 +27,7 @@ public class DerbyUtil {
     }
 
     public static void close(Statement statement, Connection conn) {
+//        每次使用之后必须关闭，在此手动提交事务
         try {
             statement.close();
             conn.commit();
